80e8f1a3cc843c2f22dcc3dbc97bd9237ba14d9e
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-02-02 15:54:04 +0530

    optimized code

---
+++ frontend/src/components/notify.js
@@ -0,0 +1,12 @@
+export function SendToast(message){
+    toast.error(message, {
+        position: "top-right",
+        autoClose: 2000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+}
