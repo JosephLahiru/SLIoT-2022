b83f8e8ed355a4348df6e2835bb2b62835f1226b
theAlphaGuardian <44818405+JosephLahiru@users.noreply.github.com>
2023-02-03 20:32:43 +0530

    added image for the 404 file

--- frontend/src/components/nomatch.js
+++ frontend/src/components/nomatch.js
@@ -1,3 +1,5 @@
+import _404 from './../images/404.jpg';
+
 export const NoMatch = () => {
-    return <center><h1>404 Page Not Found</h1></center>
+    return <center><br/><h1>404 Page Not Found.</h1><br/><img src={_404} alt="404 Not Found...!!!" style={{'width':'600px'}}/></center>
 }
